9038c908b107689da07713685c565a73f0e76e76
knh4437 <knh4437@naver.com>
2022-04-06 15:23:00 +0900

    LoginBoard

--- src/components/LoginBoard.vue
+++ src/components/LoginBoard.vue
@@ -2,32 +2,53 @@
     <div id="loginboard">
         <b-container>
             <b-row align-v="center" align-h="center">
-            <b-card no-body="no-body" style="max-width: 540px;">
-                <b-card title="로그인 / 회원가입" sub-title="KaKao Login">
-                    <b-card-text>
-                        카카오톡과 연동되어 계정 로그인 / 생성을 진행합니다.
-                    </b-card-text>
-                    <b-button>
-                        <router-link to="" style="text-decoration: none;">Login / Register</router-link>
-                    </b-button>
-                    <b-button>
-                        <router-link to="/" style="text-decoration: none;">뒤로가기</router-link>
-                    </b-button>
+                <b-card id="bcard" no-body>
+                    <b-card id="subcard" title="로그인 / 회원가입" sub-title="KaKao Login" bg-variant="light">
+                        <b-card-text>
+                            카카오톡과 연동되어 계정 로그인 / 생성을 진행합니다.
+                        </b-card-text>
+                        <b-button variant="outline-warning" @click="LoginbuttonClicked()">
+                            Login/Register
+                        </b-button>
+                        <b-button variant="outline-warning">
+                            <router-link to="/">뒤로가기</router-link>
+                        </b-button>
+                    </b-card>
                 </b-card>
-            </b-card>
             </b-row>
         </b-container>
     </div>
 </template>
 
+<script src="//developers.kakao.com/sdk/js/kakao.js"></script>
 <script>
 export default {
-  name: 'LoginBoard'
+  name: 'LoginBoard',
+  methods: {
+    LoginbuttonClicked () {
+      const param = {
+        redirectUri: 'localhost:8080/#/'
+      }
+      window.Kakao.Auth.authorize(param)
+    }
+  }
 }
 </script>
 
 <style scoped>
     #loginboard {
         height: 100vh;
+    }
+    #bcard {
+        max-width: 540px;
+        margin-top: 25%;
+        border: 0px;
+    }
+    button {
+        background-color: white;
+    }
+    a {
+        text-decoration: none;
+        color: black;
     }
 </style>
