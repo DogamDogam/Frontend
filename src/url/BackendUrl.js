070e04bb9cd6d192e8c7aa6f18e4da6515efbeb2
hy <75581904+dhgkdud12@users.noreply.github.com>
2022-05-25 09:58:14 +0900

    domain test

--- src/url/BackendUrl.js
+++ src/url/BackendUrl.js
@@ -1,1 +1,3 @@
-export const URL = 'http://13.124.189.169'
+// export const URL = 'https://13.124.189.169'
+// export const URL = 'http://localhost:9090'
+export const URL = 'http://dogamdogam.shop'
