f4227de10ec90cc2db871040381efb06b078a296
hy <75581904+dhgkdud12@users.noreply.github.com>
2022-05-25 10:09:56 +0900

    Revert "domain test"
    This reverts commit 070e04bb9cd6d192e8c7aa6f18e4da6515efbeb2.

--- src/url/BackendUrl.js
+++ src/url/BackendUrl.js
@@ -1,3 +1,1 @@
-// export const URL = 'https://13.124.189.169'
-// export const URL = 'http://localhost:9090'
-export const URL = 'http://dogamdogam.shop'
+export const URL = 'http://13.124.189.169'
